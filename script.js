8ae13219f9f96c256e72b109bc6fe714555c1649
Likitha T A <141708237+likitha1125@users.noreply.github.com>
2023-08-30 12:59:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+window.addEventListener("scroll", function() {
+    if (window.scrollY > 100) {
+      document.getElementById("scroll").style.display = "block";
+    } else {
+      document.getElementById("scroll").style.display = "none";
+    }
+  });
+  
+ 
+
